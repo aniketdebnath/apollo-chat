3608fdba6935e38ca655e357f96559b2b4e42f9b
aniketdebnath <rkstaraniket34@gmail.com>
2025-05-28 22:31:29 +1000

    nestjs backend with auth,guard,mongodb,repository,graphql resolver & ui with login,signup, hooks

---
+++ apollochat-ui/src/components/auth/Guard.tsx
@@ -0,0 +1,19 @@
+import { excludedRoutes } from "../../constants/excluded-routes";
+import { useGetMe } from "../../hooks/useGetMe";
+
+interface GuardProps {
+  children: JSX.Element;
+}
+
+const Guard = ({ children }: GuardProps) => {
+  const { data: user } = useGetMe();
+  console.log(user);
+  return (
+    <>
+      {excludedRoutes.includes(window.location.pathname)
+        ? children
+        : user && children}
+    </>
+  );
+};
+export default Guard;
