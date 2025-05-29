c76b74f0938a9ffe0d16f4f53b8d4ad0492a5aaf
aniketdebnath <rkstaraniket34@gmail.com>
2025-05-29 15:55:17 +1000

    header, logout redirect, authredirect

--- apollochat-ui/src/components/home/Home.tsx
+++ apollochat-ui/src/components/home/Home.tsx
@@ -1,4 +1,4 @@
 const Home = () => {
-  return <h1>wdawd</h1>;
+  return <h1>Home</h1>;
 };
 export default Home;
--- apollochat-ui/src/constants/apollo-client.ts
+++ apollochat-ui/src/constants/apollo-client.ts
@@ -1,8 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { excludedRoutes } from "./excluded-routes";
+import router from "../components/Routes";
+const logoutLink = onError((error) => {
+  if (
+    error.graphQLErrors?.length &&
+    (error.graphQLErrors[0]?.extensions?.originalError as any).statusCode ===
+      401
+  ) {
+    if (!excludedRoutes.includes(window.location.pathname)) {
+      router.navigate("/login");
+      client.resetStore();
+    }
+  }
+});
+const httpLink = new HttpLink({ uri: `/graphql` });
 
 const client = new ApolloClient({
-  uri: `/graphql`,
   cache: new InMemoryCache(),
+  link: logoutLink.concat(httpLink),
 });
 
 export default client;
