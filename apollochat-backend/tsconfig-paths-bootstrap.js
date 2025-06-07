ae608df29f93b56192277cd2eb1fdd0ed39a819d
aniketdebnath <rkstaraniket34@gmail.com>
2025-06-07 14:01:36 +1000

    tsconfig paths bootstrap file

---
+++ apollochat-backend/tsconfig-paths-bootstrap.js
@@ -0,0 +1,10 @@
+const tsConfigPaths = require('tsconfig-paths');
+const { compilerOptions } = require('./tsconfig.json');
+
+// This is needed to map the paths from tsconfig.json to the compiled js files
+tsConfigPaths.register({
+  baseUrl: './dist',
+  paths: {
+    '*': ['*'],
+  },
+});
